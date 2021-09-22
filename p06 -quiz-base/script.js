e15d2c692ed3ee1ddbb8e38739d6a14edb7ff0a2
WallaceSilva <wallacewvinfo@gmail.com>
2021-09-22 18:05:24 -0300

    Finishing project and doing the last commit

--- p06 -quiz-base/script.js
+++ p06 -quiz-base/script.js
@@ -4,13 +4,15 @@ let correctAnswers = 0;
  
 showQuestion();
 
+// Events
+ document.querySelector('.scoreArea button').addEventListener('click', resetEvent);
+
 // Functions
 function showQuestion() {
     if (questions[currentQuestion]) {
         let q = questions[currentQuestion];
 
         let pct = Math.floor((currentQuestion / questions.length) * 100);
-        
         document.querySelector('.progress--bar').style.width = `${pct}%`;
 
         document.querySelector('.scoreArea').style.display = 'none';
@@ -44,5 +46,30 @@ function optionClickEvent(e){
 }
 
 function finishQuiz() {
-    
+    let points = Math.floor((correctAnswers / questions.length) * 100);
+
+    if (points < 30) {
+        document.querySelector('.scoreText1').innerHTML = 'Tá ruim einh!!';
+        document.querySelector('.scorePct').style.color = '#FF0000';
+    }else if (points >= 30 && points < 70) {
+        document.querySelector('.scoreText1').innerHTML = 'Muito bom!!';
+        document.querySelector('.scorePct').style.color = '#FFFF00';
+    }else if (points >= 70) {
+        document.querySelector('.scoreText1').innerHTML = 'Parabéns!!';
+        document.querySelector('.scorePct').style.color = '#0D630D';
+    }
+
+    document.querySelector('.scorePct').innerHTML = `Acertou ${points}%`;
+    document.querySelector('.scoreText2').innerHTML = `Você respondeu ${questions.length} questões e acertou ${correctAnswers}.`;
+
+    document.querySelector('.scoreArea').style.display = 'block';
+    document.querySelector('.questionArea').style.display = 'none';
+    document.querySelector('.progress--bar').style.width = '100%';
+
+}
+
+function resetEvent() {
+    correctAnswers = 0;
+    currentQuestion = 0;
+    showQuestion();
 }
