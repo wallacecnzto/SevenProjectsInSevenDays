55359653f1b5483205f39728885264bbaabf1457
WallaceSilva <wallacewvinfo@gmail.com>
2021-09-21 21:48:58 -0300

    Add project Quiz-base(W.I.P.)

---
+++ p06 -quiz-base/script.js
@@ -0,0 +1,48 @@
+// Initial Data
+let currentQuestion = 0;
+let correctAnswers = 0;
+ 
+showQuestion();
+
+// Functions
+function showQuestion() {
+    if (questions[currentQuestion]) {
+        let q = questions[currentQuestion];
+
+        let pct = Math.floor((currentQuestion / questions.length) * 100);
+        
+        document.querySelector('.progress--bar').style.width = `${pct}%`;
+
+        document.querySelector('.scoreArea').style.display = 'none';
+        document.querySelector('.questionArea').style.display = 'block';
+
+        document.querySelector('.question').innerHTML = q.question;
+        let optionsHtml = '';
+        for(let i in q.options) {
+            optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span>${q.options[i]}</div>`;
+        }
+        document.querySelector('.options').innerHTML = optionsHtml;
+
+        document.querySelectorAll('.options .option').forEach(item => {
+            item.addEventListener('click', optionClickEvent);
+        })
+
+    }else {
+        finishQuiz();
+    }
+}
+
+function optionClickEvent(e){
+    let clickedOption = parseInt(e.target.getAttribute('data-op'));
+
+    if(questions[currentQuestion].answer === clickedOption) {
+        correctAnswers++;
+    }
+
+    currentQuestion++;
+    showQuestion();
+}
+
+function finishQuiz() {
+    
+}
